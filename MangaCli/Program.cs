4c4f75c2c47ae79f6d279d78fdaf6e4f3524cf65
Andrea Bertin <andrea.bertin@outlook.com>
2019-03-15 17:32:33 +0100

    Added solution and main projects

---
+++ MangaCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MangaCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
